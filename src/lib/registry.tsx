758a9c339c9c4cb63b8a3143c65d75c7917db3d8
Gabriel Bursi <gabriellbursii@gmail.com>
2023-12-29 23:02:41 -0300

    fix tipo da prop children

--- src/lib/registry.tsx
+++ src/lib/registry.tsx
@@ -1,12 +1,10 @@
-import React, { useState } from 'react'
+import React, { PropsWithChildren, useState } from 'react'
 import { useServerInsertedHTML } from 'next/navigation'
 import { ServerStyleSheet, StyleSheetManager } from 'styled-components'
 
 export default function StyledComponentsRegistry({
 	children
-}: Readonly<{
-	children: React.ReactNode
-}>) {
+}: Readonly<PropsWithChildren>) {
 	// Only create stylesheet once with lazy initial state
 	// x-ref: https://reactjs.org/docs/hooks-reference.html#lazy-initial-state
 	const [styledComponentsStyleSheet] = useState(() => new ServerStyleSheet())
